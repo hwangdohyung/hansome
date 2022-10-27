8d819ab01ea32a79e2b692dcaa18652e76de7f9b
do <fire211@naver.com>
2022-10-27 17:43:10 +0900

    d

--- crnn/modules/feature_extraction.py
+++ crnn/modules/feature_extraction.py
@@ -26,7 +26,7 @@ class VGG_FeatureExtractor(nn.Module):
 
     def forward(self, input):
         return self.ConvNet(input)
-
+  
 
 class RCNN_FeatureExtractor(nn.Module):
     """ FeatureExtractor of GRCNN (https://papers.nips.cc/paper/6637-gated-recurrent-convolution-neural-network-for-ocr.pdf) """
